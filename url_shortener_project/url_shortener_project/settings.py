7b128f0e978ebf893f1fffb2daa6c5d61beeba16
D8_62964_PranobKale <pranob.kale@decimalpointanalytics.com>
2025-01-26 00:03:10 +0530

    implementing .env file for secrets

--- url_shortener_project/url_shortener_project/settings.py
+++ url_shortener_project/url_shortener_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +22,10 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,"shortener.templates")
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2xwjm!7oe$ja(k3i-=bg4qxqq+txa1dgj#562z!ab3ujd5l$#*'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = ['*']  # Update this for production with specific hosts
 
